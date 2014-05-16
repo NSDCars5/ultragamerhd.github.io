d45ce8c3effc9d6a2ace3b9bde63d8f79e071907
UltraGamerHD <usaamah14@gmail.com>
2014-05-16 17:23:25 +0100

    Fix Page Not Switching
    Thanks to XDA Member @NSDCars5

--- stuff.js
+++ stuff.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	var a = new Showdown.converter();
 	$.get("home.md",function(resp){$("main").html(a.makeHtml(resp));});
-	$("home-link").click(function(){ loadPage("home"); });
-	$("kernels-link").click(function(){ loadPage("kernels"); });
-	$("roms-link").click(function(){ loadPage("roms"); });
-	$("guides-link").click(function(){ loadPage("guides"); });
+	$("#home-link").click(function(){ loadPage("home"); });
+	$("#kernels-link").click(function(){ loadPage("kernels"); });
+	$("#roms-link").click(function(){ loadPage("roms"); });
+	$("#guides-link").click(function(){ loadPage("guides"); });
 	function loadPage(page) {
 		$.get(page+".md",function(resp){
 				$("main").fadeOut(500).html(a.makeHtml(resp)).fadeIn(500);
